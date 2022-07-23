e03be85cea66d37a2f0a3d3c800b3cc9a13319bd
Aryo Budi D.P <aryobd@gmail.com>
2022-07-23 16:31:34 +0700

    20220723SAT

--- abdp_map_google/lib/main.dart
+++ abdp_map_google/lib/main.dart
@@ -29,11 +29,13 @@ class MapSample extends StatefulWidget {
 class MapSampleState extends State<MapSample> {
   final Completer<GoogleMapController> _controller = Completer();
   final List<Marker> _lstMarker = [];
+  final List<Circle> _lstCircle = [];
+  final List<LatLng> _points = [];
   Uint8List? markerIcon;
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     // target: LatLng(37.42796133580664, -122.085749655962),
-    target: LatLng(-6.175268555139486, 106.82713374020734), // ABDP
+    target: LatLng(-6.175268555139486, 106.82713374020734), // MONAS - ABDP
     // zoom: 14.4746,
     zoom: 12, // ABDP
   );
@@ -41,7 +43,7 @@ class MapSampleState extends State<MapSample> {
   static const CameraPosition _kLake = CameraPosition(
     bearing: 192.8334901395799,
     // target: LatLng(37.43296265331129, -122.08832357078792),
-    target: LatLng(-6.360980580587045, 106.74806032068038), // ABDP
+    target: LatLng(-6.360980580587045, 106.74806032068038), // WATES - ABDP
     // tilt: 59.440717697143555,
     // zoom: 19.151926040649414,
     tilt: 60, // ABDP
@@ -58,6 +60,38 @@ class MapSampleState extends State<MapSample> {
   //   draggable: true,
   // );
 
+  // Circle _circle = Circle(
+  //   circleId: CircleId("Cicle 1"),
+  //   radius: 250,
+  //   strokeColor: Colors.red,
+  //   strokeWidth: 5,
+  //   fillColor: Colors.redAccent.withOpacity(0.1),
+  //   center: LatLng(-6.360980580587045, 106.74806032068038),
+  // );
+
+  static final Polyline _polyline = Polyline(
+    polylineId: PolylineId("Polyline 1"),
+    points: const [
+      LatLng(-6.360980580587045, 106.74806032068038), // WATES - ABDP
+      LatLng(-6.360980580587045, 106.77), // ABDP
+    ],
+    color: Colors.red,
+    width: 5,
+  );
+
+  static final Polygon _polygon = Polygon(
+    polygonId: PolygonId("Polygon 1"),
+    points: const [
+      LatLng(-6.175268555139486, 106.82713374020734), // MONAS - ABDP
+      LatLng(-6.175268555139486, 106.82713374020734 + 0.03),
+      LatLng(-6.175268555139486 + 0.03, 106.82713374020734 + 0.03),
+      LatLng(-6.175268555139486 + 0.03, 106.82713374020734),
+    ],
+    strokeColor: Colors.blue,
+    strokeWidth: 2,
+    fillColor: Colors.red.withOpacity(0.2),
+  );
+
   Future<Uint8List> getMarkerIcon(String image, int size) async {
     ByteData bytData = await rootBundle.load(image);
     ui.Codec codec = await ui.instantiateImageCodec(
@@ -72,11 +106,23 @@ class MapSampleState extends State<MapSample> {
 
   @override
   Widget build(BuildContext context) {
+    Polyline _polylines = Polyline(
+      polylineId: PolylineId("Polyline 1"),
+      points: _points,
+      color: Colors.red,
+      width: 5,
+    );
+
     return Scaffold(
       // Call GoogleMap() for rendering Google Map screen // ABDP
       body: GoogleMap(
         // markers: {_marker},
         markers: Set<Marker>.from(_lstMarker), // ABDP
+        // circles: {_circle},
+        circles: Set<Circle>.from(_lstCircle), // ABDP
+        // polylines: {_polyline},
+        polylines: {_polylines},
+        // polygons: {_polygon},
         mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
@@ -113,6 +159,8 @@ class MapSampleState extends State<MapSample> {
           // final Uint8List newIcon =
           //     await getMarkerIcon('assets/images/map-marker-outline-24.png', 10);
 
+          _points.add(LatLng(position.latitude, position.longitude));
+
           _lstMarker.add(Marker(
             // icon: BitmapDescriptor.fromBytes(newIcon),
             markerId: MarkerId("Marker ${_lstMarker.length}"),
@@ -123,6 +171,16 @@ class MapSampleState extends State<MapSample> {
             ),
             draggable: true,
           ));
+
+          // _lstCircle.add(Circle(
+          //   circleId: CircleId("Circle ${_lstCircle.length}"),
+          //   radius: 250,
+          //   strokeColor: Colors.red,
+          //   strokeWidth: 5,
+          //   fillColor: Colors.redAccent.withOpacity(0.2),
+          //   center: LatLng(position.latitude, position.longitude),
+          // ));
+
           setState(() {});
         },
       ),
@@ -136,6 +194,7 @@ class MapSampleState extends State<MapSample> {
 
   Future<void> _goToTheLake() async {
     final GoogleMapController controller = await _controller.future;
+
     controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
